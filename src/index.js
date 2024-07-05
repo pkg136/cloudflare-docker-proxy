9b7abc5a268a3a976482050624d6587f6804473c
pkg136 <139035169+pkg136@users.noreply.github.com>
2024-07-05 16:46:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.9911666.xyz": dockerHub,
+  "quay.9911666.xyz": "https://quay.io",
+  "gcr.9911666.xyz": "https://gcr.io",
+  "k8s-gcr.9911666.xyz": "https://k8s.gcr.io",
+  "k8s.9911666.xyz": "https://registry.k8s.io",
+  "ghcr.9911666.xyz": "https://ghcr.io",
+  "cloudsmith.9911666.xyz": "https://docker.cloudsmith.io",
+  "ecr.9911666.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.9911666.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
